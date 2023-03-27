6fb0abc6c54d9757926c3650e0bdcb0dd3161262
EBosen <ethbosen@gmail.com>
2023-03-26 23:42:03 -0400

    Function addition to WebApp

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,14 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-
+function listTopics() {
+    for (var x=0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
+}
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
 }
 
-/*
+function selectTopic() {
 if (topic === 'HTML') {
     console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -15,5 +19,5 @@ if (topic === 'HTML') {
     console.log("Let's study JavaScript!");
 } else {
     console.log('Please try again!');
+    }
 }
-*/
